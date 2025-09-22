9e2c42b0793aee213c84198570b30f92c20559b1
Jeevan DC <jeevan.9571@gmail.com>
2025-09-22 22:37:21 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,18 @@
 #include <iostream>
 
 int main() {
-    StringCalculator calc;
+  StringCalculator calc;
 
-    std::cout << calc.Add("") << "\n";                  // 0
-    std::cout << calc.Add("1") << "\n";                 // 1
-    std::cout << calc.Add("1,2") << "\n";               // 3
-    std::cout << calc.Add("1\n2,3") << "\n";            // 6
-    std::cout << calc.Add("//;\n1;2") << "\n";          // 3
-    std::cout << calc.Add("//[***]\n1***2***3") << "\n"; // 6
-    std::cout << calc.Add("2,1001") << "\n";            // 2
-
-    try {
-        std::cout << calc.Add("1,-2,3,-4") << "\n";
-    } catch (const std::exception& ex) {
-        std::cerr << "Exception: " << ex.what() << "\n"; // negatives not allowed: -2 -4
-    }
-
-    return 0;
+  try {
+    std::cout << "Empty string: " << calc.Add("") << std::endl;  // should print 0
+    std::cout << "\"1\": " << calc.Add("1") << std::endl;  // should print 1
+    std::cout << "\"1,2\": " << calc.Add("1,2") << std::endl;  // should print 3
+    std::cout << "\"1\\n2,3\": " << calc.Add("1\n2,3") << std::endl;  // should print 6
+    std::cout << "\"//;\\n1;2\": " << calc.Add("//;\n1;2") << std::endl;  // should print 3
+    std::cout << "\"2,1001\": " << calc.Add("2,1001") << std::endl;  // should print 2
+    std::cout << "\"//[***]\\n1***2***3\": " << calc.Add("//[***]\n1***2***3") << std::endl;  // 6
+  } catch (const std::invalid_argument& ex) {
+    std::cerr << "Exception: " << ex.what() << std::endl;
+  }
+  return 0;
 }
