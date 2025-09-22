bea589a2063e9012bb689178bf79b056e9b7b256
Jeevan DC <jeevan.9571@gmail.com>
2025-09-22 22:47:28 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@ int main() {
   StringCalculator calc;
 
   try {
-    std::cout << "Empty string: " << calc.Add("") << std::endl;  // should print 0
-    std::cout << "\"1\": " << calc.Add("1") << std::endl;  // should print 1
-    std::cout << "\"1,2\": " << calc.Add("1,2") << std::endl;  // should print 3
-    std::cout << "\"1\\n2,3\": " << calc.Add("1\n2,3") << std::endl;  // should print 6
-    std::cout << "\"//;\\n1;2\": " << calc.Add("//;\n1;2") << std::endl;  // should print 3
-    std::cout << "\"2,1001\": " << calc.Add("2,1001") << std::endl;  // should print 2
-    std::cout << "\"//[***]\\n1***2***3\": " << calc.Add("//[***]\n1***2***3") << std::endl;  // 6
+    std::cout << "Empty string: " << calc.Add("") << std::endl;  // 0
+    std::cout << "\"1\": " << calc.Add("1") << std::endl;         // 1
+    std::cout << "\"1,2\": " << calc.Add("1,2") << std::endl;     // 3
+    std::cout << "\"1\\n2,3\": " << calc.Add("1\n2,3") << std::endl;  // 6
+    std::cout << "\"//;\\n1;2\": " << calc.Add("//;\n1;2") << std::endl;  // 3
+    std::cout << "\"2,1001\": " << calc.Add("2,1001") << std::endl;        // 2
+    std::cout << "\"//[***]\\n1***2***3\": "
+              << calc.Add("//[***]\n1***2***3") << std::endl;  // 6
   } catch (const std::invalid_argument& ex) {
     std::cerr << "Exception: " << ex.what() << std::endl;
   }
